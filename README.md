7cc5f55a469ce0a1bbc907e688bb30a1cc52ad3c
Johnny Rice <jrice@labact.com>
2020-07-22 18:40:35 -0400

    Provide starting for week 08

---
+++ README.md
@@ -0,0 +1,3 @@
+# Week 08 Array Methods
+
+Just like last week. More samples and coding together.
---
+++ index.js
@@ -0,0 +1,50 @@
+/**
+
+    We will push to github all along the way so we track our week and keep a log
+
+    Declaring an Array with Objects
+
+    1)  Declare and initialize an array of `assignments` with `name` and `completed` properties. Name the array `assignments`.  `completed` will have boolean values.
+
+ */
+
+
+ /**
+
+    Referencing code in other files
+ 
+    2a)  Great work! Now move that array initialization out into its own file. 
+        See ./data/assignments.js where you will initialize the data in place of inline in this function
+
+    2b) "Import" that data into this file in place of the inline code you had in step 1
+    
+
+  */
+
+
+/**
+
+    Looping and using references to arrays a given positions
+ 
+    3) Loop through the data using a for loop. Just console.log within the loop to show each item within the array
+
+ */
+
+/**
+
+    Looping and Assignment
+ 
+    4) Declare a new array named `allAssignments`. Loop through the `assignments` array data using a for loop and assign each item from `assignments` to the new `allAssignments` array
+
+ */
+
+
+/**
+
+    Filtering
+ 
+    5) Declare a new array named `completedAssignments`. Loop through the `assignments` array data using a for loop.  Use a condition to only add to `allAssignments` where there are property values with `completed` of `true`
+
+ */
+
+
