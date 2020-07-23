74f5b4f17b38b3814455477ef10e1272c36a8dda
Alpha Comara <saliouc@yahoo.com>
2020-07-23 18:46:31 -0400

    Week-08 assigment

--- index.js
+++ index.js
@@ -7,16 +7,19 @@
     1)  Declare and initialize an array of `assignments` with `name` and `completed` properties. Name the array `assignments`.  `completed` will have boolean values.
 
  */
-let assignments=[{
-name : 'deva',
-completed: true}]
+const assignments=[{name : 'name',completed: true}]
 
- /**
+ 
+import { assignments } from './data/assigments'
+console.log(assignments)
+/**
 
     Referencing code in other files
  
     2a)  Great work! Now move that array initialization out into its own file. 
         See ./data/assignments.js where you will initialize the data in place of inline in this function
+    
+
 
     2b) "Import" that data into this file in place of the inline code you had in step 1
     
@@ -31,6 +34,18 @@ completed: true}]
     3) Loop through the data using a for loop. Just console.log within the loop to show each item within the array
 
  */
+for(let index in assignments){
+
+    const assigment = assignments[index]
+    console.log(index,assignment)
+}
+
+for(let index =0; index < assignments.length;index +1){
+const assigment = assignments[index]
+cosnsole.log({ assigment })
+}
+
+
 
 /**
 
@@ -39,6 +54,17 @@ completed: true}]
     4) Declare a new array named `allAssignments`. Loop through the `assignments` array data using a for loop and assign each item from `assignments` to the new `allAssignments` array
 
  */
+let AllAssigment=[]
+for(let index = 0; index < assignments.length;index +1){
+const assigment = assignments[index]
+AllAssigments.push(assigment)}
+
+console.log({AllAssigments})
+
+let allAssignments = assignments.map((assigment)=>{
+return assigment
+})
+console.log(allAssignments)
 
 
 /**
@@ -50,3 +76,4 @@ completed: true}]
  */
 
 
+module.export = assignments
